2df594f3ea9e5a142c4410970994c94c056dab38
Dhanwanth M <78773231+dhanwanthm@users.noreply.github.com>
2021-02-10 20:12:22 +0530

    Create EvenNumbersFromMtoN.c

---
+++ EvenNumbersFromMtoN.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int even(int m, int n)
+{
+    int i;
+    printf("The even numbers from %d to %d are:\n", m, n);
+    if(m>n)
+        printf("Error");
+    else
+    for(i=m; i<=n; i++)
+    {
+        if(i%2==0)
+            printf("%d\n", i);
+    }
+}
+int main()
+{
+    int a,b,e;
+    printf("Enter the values from which to which the even numbers has to be printed\n");
+    scanf("%d %d", &a, &b);
+    e=even(a,b);
+}
